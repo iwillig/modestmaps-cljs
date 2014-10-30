2a55ba9ed7d20d7be7b9e4f8737fb5def725b27a
Ivan Willig <iwillig@gmail.com>
2014-10-30 16:59:05 -0400

    REBASE ME

--- src/modestmaps/tile.clj
+++ src/modestmaps/tile.clj
@@ -7,18 +7,26 @@
   {:pre  [(>= zoom 0)]}
   (reduce (fn [n i] (+ n (Math/pow 4 i))) 0 (range (+ 1 zoom))))
 
+(defn generate-parents [coord])
 
 (defn seed-tiles
-  [{:keys [zoom-start zoom-end] :or {zoom-start 0 zoom-end 2}}]
+  [{:keys [zoom-start zoom-end] :or {zoom-start 0 zoom-end 3}}]
   (for [zoom (range zoom-start (+ 1 zoom-end))
         :let [limit (Math/pow 2 zoom)]]
     (for [col (range limit)]
       (for [row (range limit)]
         (->Coordinate zoom col row)))))
 
+;; http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
 (defn num->deg
-  [x y zoom])
+  [xtile ytile zoom]
+  (let [n        (Math/pow 2 zoom)
+        long-deg (/ xtile (- (* n 360 ) 180))
+        lat-rad  (Math/atan (Math/sinh (* Math/PI  (- 1 (/ n (* 2 ytile))))))
+        lat-deg  (Math/toDegrees lat-rad)]
+    [lat-deg long-deg]))
 
+;; http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
 (defn deg->num
   [lat lon zoom])
 
