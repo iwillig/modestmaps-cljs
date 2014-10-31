e117f1907bf1481ae3e73f1df08c92abad53201a
Ivan Willig <iwillig@gmail.com>
2014-10-31 08:22:30 -0400

    Port more of Robs tile stuff to clj

--- src/modestmaps/tile.clj
+++ src/modestmaps/tile.clj
@@ -10,7 +10,8 @@
 (defn generate-parents [coord])
 
 (defn seed-tiles
-  [{:keys [zoom-start zoom-end] :or {zoom-start 0 zoom-end 3}}]
+  [{:keys [zoom-start zoom-end]
+    :or {zoom-start 0 zoom-end 3}}]
   (for [zoom (range zoom-start (+ 1 zoom-end))
         :let [limit (Math/pow 2 zoom)]]
     (for [col (range limit)]
@@ -26,15 +27,59 @@
         lat-deg  (Math/toDegrees lat-rad)]
     [lat-deg long-deg]))
 
+
 ;; http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
 (defn deg->num
-  [lat lon zoom])
+  [lat lon zoom]
+  (let [lat-rad (Math/toRadians lat)
+        n       (Math/pow 2 zoom)
+        xtile   (* (/ (+ lat 180)
+                      360)
+                   n)
+;;     int((1.0 - math.log(math.tan(lat_rad) + (1 / math.cos(lat_rad))) / math.pi) / 2.0 * n)
+        ytile   (* (/ (/ (- 1 (Math/log (+
+                                            (Math/tan lat-rad)
+                                            (/ 1 (Math/cos lat-rad)))))
+                          Math/PI) 2) n)
+        ]
+    [xtile ytile]))
+
 
 (defn coord->bounds
-  [coord])
+  [coord]
+  (let [[top-left-lat, top-left-lng] (num->deg (:column coord)
+                                               (:row    coord)
+                                               (:zoom   coord))
+        [bottom-right-lat bottom-right-lng] (num->deg (+ 1 (:column coord))
+                                                      (+ 1 (:row coord))
+                                                      (:zoom coord))
+        minx top-left-lng
+        miny bottom-right-lat
+        maxx (Math/min 180 bottom-right-lng)
+        maxy (Math/min 90  top-left-lat)]
+    [minx miny maxx maxy]))
+
 
 (defn bounds->coords
-  [coord])
+  [bounds zoom]
+  (let [[minx miny maxx maxy] bounds
+        top-left-lng minx
+        top-left-lat maxy
+        bottom-right-lat miny
+        bottom-right-lng maxx
+        [top-left-x top-left-y] (deg->num top-left-lat top-left-lng zoom )
+        [bottom-right-x bottom-right-y] (deg->num bottom-right-lat bottom-right-lng zoom)
+        maxval (- (Math/pow 2 zoom) 1)
+        bottom-right-x (Math/min maxval bottom-right-x)
+        bottom-right-y (Math/min maxval bottom-right-y)
+        top-left-coord (->Coordinate top-left-y top-left-x zoom)]
+
+    (if (and (= top-left-x bottom-right-x)
+             (= top-left-y bottom-right-y))
+      [top-left-coord]
+      [top-left-coord (->Coordinate bottom-right-y
+                                    bottom-right-x
+                                    zoom)])))
 
 (defn generate-tile-for-single-bounds
   [bounds zoom-start zoom-unitl])
